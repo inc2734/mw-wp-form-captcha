bba016dcb860df16b80eda16481c99458176c95b
Takashi Kitajima <inc@2inc.org>
2019-08-30 13:15:04 +0900

    1.4.0

--- mw-wp-form-captcha.php
+++ mw-wp-form-captcha.php
@@ -3,11 +3,11 @@
  * Plugin Name: MW WP Form CAPTCHA
  * Plugin URI: http://plugins.2inc.org/mw-wp-form/
  * Description: Adding CAPTCHA field on MW WP Form. This plugin needs MW WP Form version 2.2.0 or later.
- * Version: 1.3.2
+ * Version: 1.4.0
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Created : August 10, 2014
- * Modified: December 14, 2015
+ * Modified: August 30, 2019
  * Text Domain: mw-wp-form-captcha
  * Domain Path: /languages/
  * License: GPLv2
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.amazon.co.jp/registry/wishlist/39ANKRNSTNW40
 Tags: plugin, form, mail, captcha
 Requires at least: 3.7
 Tested up to:4.3.1
-Stable tag: 1.3.2
+Stable tag: 1.4.0
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
